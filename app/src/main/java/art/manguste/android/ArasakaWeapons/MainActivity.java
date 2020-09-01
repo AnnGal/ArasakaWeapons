897df7da6f63300823cf23fc170f834eca981067
Anya Galyamova <galyamova.anya@gmail.com>
2020-09-01 13:25:15 +0300

    feat: add tabs and fragments

--- app/src/main/java/art/manguste/android/ArasakaWeapons/MainActivity.java
+++ app/src/main/java/art/manguste/android/ArasakaWeapons/MainActivity.java
@@ -1,8 +1,14 @@
 package art.manguste.android.ArasakaWeapons;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //set scroller
+        SectionPagerAdapter pagerAdapter = new SectionPagerAdapter(getSupportFragmentManager());
+        ViewPager pager = findViewById(R.id.view_pager_shop);
+        pager.setAdapter(pagerAdapter);
+
+        //set tabLayout
+        TabLayout tabLayout = findViewById(R.id.tabs_shop);
+        tabLayout.setupWithViewPager(pager);
     }
+
+    private class SectionPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0: return new WeaponListFragment();
+                case 1: return new ServiceListFragment();
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0: return getResources().getText(R.string.weapons_tab);
+                case 1: return getResources().getText(R.string.services_tab);
+            }
+            return null;
+        }
+    }
+
+    // TODO add fab "to cart"
 }
